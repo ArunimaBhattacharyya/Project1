359bbb9f91a068ef5ab4b6b4bec825d9c8b68bf4
ArunimaBhattacharyya <arunima4002.bh@gmail.com>
2026-03-09 12:11:51 +0530

    new code added

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -5,8 +5,12 @@ public class Demo {
 	public static void main(String[] args) {
 		int a = 20;
 		int b = 20;
+		
+		int c = 30;
+		int d = 20;
 		int sum = a + b;
 		System.out.println(sum);
+		System.out.println(c-d);
 	}
 
 }
